4003080a84d7816258c2e08b6ddd9009b59674aa
bxcowo <a.flores.a@uni.pe>
2026-06-22 13:31:07 -0500

    chore: Add constants for game settings.

---
+++ game/settings.py
@@ -0,0 +1,31 @@
+# Configuración de pantalla de juego
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Tamaño y cantidad de los recuadros del mundo general
+TILE_WIDTH = 120
+TILE_HEIGHT = 60
+GRID_SIZE_WIDTH = 3
+GRID_SIZE_HEIGHT = 10
+
+# Offset de muestra de recuadros
+ORIGIN_X = (SCREEN_WIDTH) / 1.3
+ORIGIN_Y = 200
+
+# Configuración de tiempos y espacios para el movimiento del jugador
+JUMP_DURATION_MS = 1100
+JUMP_VISUAL_LIFT = 40
+ROLL_DURATION_MS = 1100
+LANE_SWAP_SPEED = 10
+PLAYER_ROW = 5
+
+# Configuración de generación de obstaculos
+OBSTACLE_SPEED = 2
+SPAWN_ROW = 0
+INITIAL_STEP_MS = 800
+MIN_STEP_MS = 250
+DIFFICULTY_RAMP_MS = 20_000
+
+# Threshold para detección de colisión entre jugador y obstaculos.
+HIT_TOLERANCE = 0.4
