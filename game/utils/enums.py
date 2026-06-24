63ae21dec588b5601101eef252162beacdbaab22
bxcowo <a.flores.a@uni.pe>
2026-06-24 00:03:34 -0500

    chore: Add button classes as enum.

--- game/utils/enums.py
+++ game/utils/enums.py
@@ -11,3 +11,12 @@ class PlayerState(Enum):
     RUNNING = auto()
     JUMPING = auto()
     ROLLING = auto()
+
+
+class ButtonContent(Enum):
+    PLAY = auto()
+    QUIT = auto()
+    MAIN_MENU = auto()
+    CONTINUE = auto()
+    INFINITY_MODE = auto()
+    RESTART = auto()
