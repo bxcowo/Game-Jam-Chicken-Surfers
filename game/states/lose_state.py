5ee1f6381d9db4223067bfb82fddca7c45c4da4a
bxcowo <a.flores.a@uni.pe>
2026-06-24 00:09:52 -0500

    chore: Complete lose state with ui elements and functionalities for user experience.

--- game/states/lose_state.py
+++ game/states/lose_state.py
@@ -1,20 +1,48 @@
 import pygame
+from game.settings import SWITCH_STATE
+from game.states.base_state import State
+from game.ui.button import Button
+from game.ui.infinite_scroll_background import InfiniteScrollBackground
+from game.ui.panel import ImagePanel
+from game.utils.enums import ButtonContent
 
-class LoseState:
+
+class LoseState(State):
     def __init__(self) -> None:
+        super().__init__()
         self.font = pygame.font.SysFont(None, 48)
+        self.background = InfiniteScrollBackground("lose_bg")
+        self.lose_title = ImagePanel(x=0, y=125, key="lose_title")
+        self.restart_button = Button(x=-110, y=500, content=ButtonContent.RESTART, on_click=self._restart_action)
+        self.back_menu_button = Button(x=110, y=500, content=ButtonContent.MAIN_MENU, on_click=self._main_menu_action)
+        self.lose_image = ImagePanel(x=0, y=300, key="pollo_ala_brasa")
         self.game_over = False
 
     def enter(self) -> None:
-        pass
+        self.input_manager.subscribe(self.restart_button)
+        self.input_manager.subscribe(self.back_menu_button)
 
     def exit(self) -> None:
-        pass
+        self.restart_button.reset()
+        self.back_menu_button.reset()
+        self.input_manager.unsubscribe(self.restart_button)
+        self.input_manager.unsubscribe(self.back_menu_button)
 
     def update(self, dt: int) -> None:
-        pass
+        self.background.update(dt)
 
-    def draw(self, surface: pygame.Surface) -> None:
-        surface.fill((20, 0, 0))
-        text = self.font.render("Te comieron uu. Se acabo el juego", True, (255, 255, 255))
-        surface.blit(text, text.get_rect(center=surface.get_rect().center))
+    def draw(self, screen: pygame.Surface) -> None:
+        self.background.draw(screen)
+        self.lose_title.draw(screen)
+        self.lose_image.draw(screen)
+        self.restart_button.draw(screen)
+        self.back_menu_button.draw(screen)
+
+    def handle_event(self, event: pygame.event.Event) -> None:
+        self.input_manager.distribute(event)
+
+    def _restart_action(self):
+        pygame.event.post(pygame.event.Event(SWITCH_STATE, {"target": "playing"}))
+
+    def _main_menu_action(self):
+        pygame.event.post(pygame.event.Event(SWITCH_STATE, {"target": "menu"}))
