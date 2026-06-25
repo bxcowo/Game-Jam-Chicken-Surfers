c36cd30a657245a08f894eb70567d941df0ad8c3
Diego Dev <manuelchema999@gmail.com>
2026-06-25 00:54:01 -0500

    feat(collection-system): add _apply_effect with points and shield powerup support

--- game/systems/collection_system.py
+++ game/systems/collection_system.py
@@ -1,5 +1,6 @@
-from game.settings import PLAYER_ROW, COLLECTIBLE_VALUES
+from game.settings import PLAYER_ROW, COLLECTIBLE_VALUES, POWERUP_DURATIONS
 from game.utils.dataclasses import GameContext
+from game.utils.enums import CollectibleType
 from game.utils.resources import get_sound_effects
 
 
@@ -9,6 +10,7 @@ class CollectionSystem:
         self.collectible_group = collectible_group
         self.context = context
         self.coin_sound = get_sound_effects("coin_sound_effect")
+        self.upgrade_sound = get_sound_effects("upgradable_sound_effect")
 
     def check(self) -> None:
         for collectible in list(self.collectible_group):
@@ -16,6 +18,14 @@ class CollectionSystem:
                 continue
             if abs(collectible.gy - PLAYER_ROW) > 0.6:
                 continue
-            self.context.score += COLLECTIBLE_VALUES[collectible.collectible_type]
-            self.coin_sound.play()
+            self._apply_effect(collectible)
             collectible.kill()
+
+    def _apply_effect(self, collectible) -> None:
+        ctype = collectible.collectible_type
+        if ctype in COLLECTIBLE_VALUES:
+            self.context.score += COLLECTIBLE_VALUES[ctype]
+            self.coin_sound.play()
+        elif ctype == CollectibleType.ESCUDO:
+            self.player.shield_timer = POWERUP_DURATIONS[CollectibleType.ESCUDO]
+            self.upgrade_sound.play()
